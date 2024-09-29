c6b5706f9d9923b1395df8ce3944c0bb9e1c7618
Farshid Hashemi <farshidhashemi1997@gmail.com>
2024-09-29 23:00:07 +0330

    Delete part1/twice_reverse.py

--- part1/twice_reverse.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-reverse_num = 0
-
-while num > 0:
-    temp = num % 10
-    reverse_num = reverse_num * 10 + temp
-    num = num // 10
-multiply = reverse_num * 2
-print(multiply)
